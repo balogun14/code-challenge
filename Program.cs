b3c609c9f844eebac25f253c9bd98406898d5f4b
balogun14 <awwalbalogun06@gmail.com>
2023-11-17 04:30:58 +0100

    Add Longest Palindrome SubString

--- Program.cs
+++ Program.cs
@@ -92,4 +92,70 @@ class Program
         }
         return lastPosition == 0;
     }
+    public static void LongestPalindrome(string word)
+    {
+        int length = word.Length;
+        // All subStrings of length 1
+        // are palindromes
+        int maxLength = 1;
+        int  start = 0;
+        // using nested for loops to mark start and end index
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = i; j < word.Length; j++)
+            {
+                int flag = 1;
+
+                // Check palindrome
+                for (int k = 0; k < (j - i + 1) / 2; k++)
+                    if (word[i + k] != word[j - k])
+                        flag = 0;
+
+                // Palindrome
+                if (flag != 0 && (j - i + 1) > maxLength)
+                {
+                    start = i;
+                    maxLength = j - i + 1;
+                }
+            }
+        }
+        Console.Write("Longest palindrome substring is: ");
+        PrintSubStr(word, start, start + maxLength - 1);
+    }
+
+    private static void PrintSubStr(string word, int start, int v)
+    {
+        for (int i = start; i < v; i++)
+        {
+            Console.WriteLine(word[i]);
+            
+        }
+    }
+
+    public static bool IsValid(string parentheses)
+    {
+        /*
+        Time Complexity: o(n)
+        Space Complexity: o(n)
+        */
+        Stack<char> stack = new();
+
+        Dictionary<char, char> dict = new(){
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'}
+        };
+
+        for (int i = 0; i < parentheses.Length; i++)
+        {
+            if (dict.Keys.Contains(parentheses[i]))
+                stack.Push(parentheses[i]);
+            else if (stack.Count > 0 && parentheses[i] == dict[stack.Peek()])
+                stack.Pop();
+            else
+                return false;
+        }
+
+        return stack.Count == 0;
+    }
 }
